b2e47438f9e66c65a3e6925e7d0182e1e3b6a102
unknown <genzr05@gmail.com>
2022-08-19 13:59:27 +1000

    Internationalize service by using spring message bundles and LocaleContextHolder through Accept-Language headers

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
-import com.in28minutes.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -23,6 +28,12 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+//            @RequestHeader(name = "Accept-Language", required = false) Locale locale
+        ){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+    }
 
 
 }
